41a37a3974f1e28e4879adaa4faae8991f4a8176
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-05-15 09:47:26 -0500

    i'm sad

--- zephyr/intel.py
+++ zephyr/intel.py
@@ -3,6 +3,8 @@ from typing import Iterable
 from typing import Union
 from typing import Callable
 
+# ========== ENTITIES ========== #
+
 def levenshtein(a, b):
 	if len(b) == 0:
 		return len(a)
@@ -155,3 +157,112 @@ class Classification:
 		matches = sum([1 for ent in self if entity % ent >= self.strictness])
 		matched = maximumSimilarity >= self.strictness
 		return ent, maximumSimilarity, matched, matches
+
+# ========== NEURAL NETWORKING ========== #
+
+class Neuron:
+	def __init__(self):
+		self.bias = 0
+		self.inputs = []
+		self.accumulatedError = 0
+
+		self.inputAxons: list[object] = [] # axons that feed input to this neuron
+		self.outputAxons: list[object] = [] # axons that this neuron feeds output to
+
+	def reset(self) -> None:
+		self.inputs = []
+		self.accumulatedError = 0
+
+	def forward(self) -> None:
+		output = sum(self.inputs) + self.bias
+		for axon in self.outputAxons:
+			axon.forward(output)
+		return output
+
+	def backward(self) -> None:
+		for axon in self.inputAxons:
+			axon.backward(self.accumulatedError)
+		self.bias -= self.accumulatedError
+
+	def receive(self, value: float) -> None:
+		self.inputs.append(value)
+
+	def accumulate(self, error: float) -> None:
+		self.accumulatedError += error
+
+class Axon:
+	def __init__(self, input: Neuron, output: Neuron) -> None:
+		self.weight = 2 * (random() - 0.5)
+
+		self.input = input # the neuron that this axon is fed from
+		self.output = output # the neuron that this axon feeds
+
+		self.input.outputAxons.append(self)
+		self.output.inputAxons.append(self)
+
+	def forward(self, value: float) -> None:
+		self.output.receive(value * self.weight)
+
+	def backward(self, error: float) -> None:
+		self.input.receive(error * self.weight)
+		self.weight -= error
+
+class FeedforwardLayer:
+	def __init__(self, size: int) -> None:
+		self.neurons = []
+		for _ in range(size):
+			self.neurons.append(Neuron())
+
+	def __len__(self) -> int:
+		return len(self.neurons)
+
+	def __iter__(self) -> Iterable:
+		return iter(self.neurons)
+
+	def receive(self, inputs: list[float]) -> None:
+		for neuron in self:
+			for item in inputs:
+				neuron.receive(item)
+
+	def accumulate(self, errors: list[float]) -> None:
+		assert len(errors) == len(self), "Invalid count of errors."
+		for neuron, error in zip(self, errors):
+			neuron.accumulate(error)
+
+class FeedforwardNeuralNetwork:
+	def __init__(self, *sizes: set[int]) -> None:
+		self.layers = [FeedforwardLayer(size) for size in sizes]
+		self.axons = []
+		for index, layer in enumerate(self[:-1]):
+			for neuron in layer:
+				for otherNeuron in self[index + 1]:
+					self.axons.append(Axon(neuron, otherNeuron))
+
+	def __iter__(self) -> Iterable:
+		return iter(self.layers)
+
+	def __reversed__(self) -> Iterable:
+		return reversed(self.layers)
+
+	def __getitem__(self, index: Union[int, slice]) -> FeedforwardLayer:
+		return self.layers[index]
+
+	def forward(self, inputs: list[float]) -> list[float]:
+		self[0].receive(inputs)
+		for layer in self:
+			layer.forward()
+		return [neuron.forward() for neuron in self[-1]]
+
+	def backward(self, errors: list[float]) -> None:
+		self[-1].accumulate(errors)
+		for layer in reversed(self):
+			layer.backward()
+
+	def train(self, data: list[tuple[list[float], list[float]]], iterations: int=1000) -> None:
+		for _ in range(iterations):
+			for inputs, outputs in data:
+				prediction = self.forward(inputs)
+				errors = [predicted - output for predicted, output in zip(prediction, outputs)]
+				self.backward(errors)
+
+# ========== VECTORIZED ANALYSIS ========== #
